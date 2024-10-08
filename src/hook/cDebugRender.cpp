5fb1e6459f7c6cd1768e17c261e8b60199f577d2
Xinerki <simasadves@gmail.com>
2024-10-09 01:01:40 +0200

    correct name

--- src/hook/cDebugRender.cpp
+++ src/hook/cDebugRender.cpp
@@ -1,9 +1,9 @@
 #include <StdInc.h>
 #include "cDebugRender.h"
 
-DWORD& FontRoot = *reinterpret_cast<DWORD*>(0xE5FEEC);
+DWORD& debugrender_instance = *reinterpret_cast<DWORD*>(0xE5FEEC);
 
 void RenderText(float x, float y, tColor color, char* text, float persist_time, bool render_always, bool use_render_index, float scale)
 {
-	injector::thiscall<void(DWORD, float, float, tColor, char*, float, bool, bool, float)>::call(0xA71900, FontRoot, x, y, color, text, persist_time, render_always, use_render_index, scale);
+	injector::thiscall<void(DWORD, float, float, tColor, char*, float, bool, bool, float)>::call(0xA71900, debugrender_instance, x, y, color, text, persist_time, render_always, use_render_index, scale);
 }
